699008d4a76d6487d639c4ac9fe619eb02b02f9a
coderyu5523 <coderyu5523@naver.com>
2024-04-14 16:52:15 +0900

    password obscuretext 추가

--- lib/components/custom_text_form_field.dart
+++ lib/components/custom_text_form_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  final text ;
+  final text;
 
   CustomTextFormField({required this.text});
 
@@ -13,19 +13,24 @@ class CustomTextFormField extends StatelessWidget {
         Text(text),
         TextFormField(
           validator: (value) =>
-          value!.isEmpty ? "Please enter some text" : null,
+              value!.isEmpty ? "Please enter some text" : null,// 입력값이 없다면 출력
+          obscureText: text == "Password" ? true : false, // 비밀번호를 입력하면 **** 처리하기
           decoration: InputDecoration(
-            hintText: "Enter email",
-            enabledBorder: OutlineInputBorder( // 기본 디자인
+            hintText: "Enter ${text}",
+            enabledBorder: OutlineInputBorder(
+              // 기본 디자인
               borderRadius: BorderRadius.circular(20),
             ),
-            focusedBorder: OutlineInputBorder( // 손가락 터치시 디자인
+            focusedBorder: OutlineInputBorder(
+              // 손가락 터치시 디자인
               borderRadius: BorderRadius.circular(20),
             ),
-            errorBorder: OutlineInputBorder( //에러 발생시
+            errorBorder: OutlineInputBorder(
+              //에러 발생시
               borderRadius: BorderRadius.circular(20),
             ),
-            focusedErrorBorder: OutlineInputBorder( // 에러 발생 후 손가락 터치했을 때
+            focusedErrorBorder: OutlineInputBorder(
+              // 에러 발생 후 손가락 터치했을 때
               borderRadius: BorderRadius.circular(20),
             ),
           ),
